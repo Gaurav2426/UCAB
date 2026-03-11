38d22166050dccf680845b33b455660a620c09be
Gaurav2426 <gaurav.2428cseaiml10@kiet.edu>
2026-03-11 22:22:44 +0530

    Fix CORS by allowing Vercel URL explicitly

--- server/server.js
+++ server/server.js
@@ -13,19 +13,12 @@ connectDB();
 const app = express();
 
 // --------------- Middleware ---------------
-const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.CLIENT_URL
-].filter(Boolean);
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: [
+    "http://localhost:5173", 
+    "https://ucab-lyart.vercel.app",
+    process.env.CLIENT_URL
+  ].filter(Boolean),
   credentials: true
 }));
 app.use(express.json());
